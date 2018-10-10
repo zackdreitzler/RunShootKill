2a31f1bac92db3e5fa296b69f3bd9c1e32637bf6
zackdreitzler <zdreitzler@gmail.com>
2018-10-10 16:30:48 -0400

    linked scenes and added funtionality to the buttons.

---
+++ Assets/scripts/ButtonFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour {
+
+    // loads up the main game screen.
+    //**Right now it only loads a test environment. 
+    //  This will be where we call our procedural generation of the level.**
+	public void LoadGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    // Loads the load game screen
+    public void LoadLoadMenu()
+    {
+        SceneManager.LoadScene("LoadMenu");
+    }
+
+    // This quits the application.
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // This returns you to the main menu. 
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
