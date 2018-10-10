3bb90481f0dbc5467bb7f52e6c09e1bc2b64020f
cperry92 <perryc17@students.ecu.edu>
2018-10-10 13:02:44 -0400

    initial commit

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    public float maxSpeed = 10f;
+    private Rigidbody2D player;
+
+    // Use this for initialization
+    void Start () {
+        player = this.GetComponent<Rigidbody2D>();
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool moveup = Input.GetKey(KeyCode.W);
+        bool movedown = Input.GetKey(KeyCode.S);
+        bool moveleft = Input.GetKey(KeyCode.A);
+        bool moveright = Input.GetKey(KeyCode.D);
+        Vector3 mouse = Input.mousePosition;
+        
+        float vertvel = 0f;
+        float horvel = 0f;
+
+        Vector3 screen = Camera.main.WorldToScreenPoint(transform.localPosition);
+
+        Vector2 offset = new Vector2(mouse.x - screen.x, mouse.y - screen.y);
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        player.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+        if (moveup)
+        {
+            vertvel = maxSpeed;
+        }
+        if(movedown)
+        {
+            vertvel = -maxSpeed;
+        }
+        if(moveright)
+        {
+            horvel = maxSpeed;
+        }
+        if(moveleft)
+        {
+            horvel = -maxSpeed;
+        }
+        player.velocity = new Vector2(horvel, vertvel);
+
+        
+	}
+}
---
+++ Assets/scripts/shooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+
+    public GameObject bullet;
+    private Vector2 offset;
+    public float x;
+    public float y;
+    public float velocity;
+    
+    // Use this for initialization
+    void Start () {
+        velocity = 10f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        x = Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad) * -1;
+        y = Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad); 
+        offset = new Vector2(.52f * x,.52f * y);
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject b = Instantiate(bullet, (Vector2)transform.position + offset, Quaternion.identity);
+            b.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity * x, velocity * y);
+        }
+	}
+}
