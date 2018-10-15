e7e22cf731bbf512b2f5d6fb009ea8722cafcd92
PatrickLanning <42130122+PatrickLanning@users.noreply.github.com>
2018-10-15 12:42:35 -0400

    Initial Commit. Added Enemies (#1)
    Auto Gun Enemy
    Shotgun Enemy
    Zombie
    Slime

---
+++ Run Shoot Kill/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,28 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int health;
+    public Slime slime;
+	
+	void Update () {
+		if (health <= 0 )
+        {
+            // if object is a slime, spawn smaller slime
+            if (slime != null)
+            {
+                slime.spawnChildren();
+            }
+
+            Destroy(gameObject);
+        }
+	}
+
+    public void takeDamage(int damage)
+    {
+       health -= damage;
+    }
+
+}
---
+++ Run Shoot Kill/Assets/Scripts/Zombie.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+    public Rigidbody2D zombieRB;
+    public float zombieSpeed;
+    public float zombieRotateSpeed;
+
+    private PlayerController player;
+
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+	}
+	
+    void FixedUpdate()
+    {
+        
+        transform.Translate(-Vector3.up * zombieSpeed * Time.deltaTime);
+    }
+	
+	void Update () {
+        Rotate();
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //hurt player
+    }
+
+    void Rotate()
+    {
+        Vector3 vectorToTarget = player.transform.position - transform.position;
+        float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) + 90;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * zombieRotateSpeed);
+    }
+}
