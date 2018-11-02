659c3a2bab0cf9732f5fe2ddb985685e73fd3ffc
zackdreitzler <zdreitzler@gmail.com>
2018-11-02 13:52:42 -0400

    setup shop functionality and UILayout.

---
+++ Assets/scripts/UI/Shop/ShopButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopButtons : MonoBehaviour {
+    
+    public void BuyArmor()
+    {
+        //Decrement player currency.
+        Debug.Log("Armor Purchased");
+    }
+
+    public void BuyHealth()
+    {
+        Debug.Log("Health Purchased");
+    }
+
+    public void pistolUpgrade()
+    {
+        Debug.Log("Upgrade purchased");
+    }
+
+}
---
+++ Assets/scripts/UI/Shop/shopControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shopControl : MonoBehaviour {
+    public GameObject shopPanel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) openShop();
+    }
+
+    private void openShop()
+    {
+        shopPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void CloseShop()
+    {
+        shopPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
