c3c6a9de39485af96dc41e394e338fcd07abea46
zackdreitzler <zdreitzler@gmail.com>
2018-11-05 21:58:21 -0500

    made teleporter work

---
+++ Assets/scripts/TeleporterController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleporterController : MonoBehaviour {
+    private int currentIndex;
+
+    //When a player enters the teleporter they are sent to the next scene.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (collision.gameObject.CompareTag("Player")){
+            SceneManager.LoadScene(currentIndex + 1);
+        }
+    }
+}
