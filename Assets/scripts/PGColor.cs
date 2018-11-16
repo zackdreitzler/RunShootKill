a13979a4cd565c64eef8a7abf48006218f9c4679
cperry92 <perryc17@students.ecu.edu>
2018-11-16 12:47:43 -0500

    wow keep breaking stuff. fixed stuff I broke

--- Assets/scripts/PGColor.cs
+++ Assets/scripts/PGColor.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class PGColor : MonoBehaviour {
     Color pColor = new Color(255f/255, 18f/255, 29f/255, 255f/255);
     Color MGColor = new Color(238f/255, 230f/255, 53f/255, 255f/255);
-<<<<<<< HEAD
     Color SGColor = new Color(32f / 255, 32f / 255, 32f / 255, 1);
-=======
->>>>>>> 8728be228b64f83329ef9f87a4cfec281bba9a3d
     SpriteRenderer sr;
 	// Use this for initialization
 	void Start () {
@@ -30,12 +27,9 @@ public class PGColor : MonoBehaviour {
         {
             sr.color = pColor;
         }
-<<<<<<< HEAD
         if (name == "Shotgun")
         {
             sr.color = SGColor;
         }
-=======
->>>>>>> 8728be228b64f83329ef9f87a4cfec281bba9a3d
     }
 }
