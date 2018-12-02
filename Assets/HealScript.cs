3b01596af5690f77053f71629740e9e725e58174
Zaben <zabe.rauf@hotmail.com>
2018-12-02 15:30:15 -0500

    healing stuff

---
+++ Assets/HealScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {	}
+	
+	// Update is called once per frame
+	void Update () { }
+
+	void OnTriggerEnter(Collider other) {
+		GameObject Player = GameObject.Find("Player");
+	
+		damagable playerhealth = currHealth.GetComponent();
+		playerhealth.currHealth += 10;
+		Destroy(GameObject);
+	}
+}
